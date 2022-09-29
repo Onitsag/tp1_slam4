659cba7340ee1e84907021b039ac012e7605f5fb
Timéo Gastineau <tgastineau@dombtsig.local>
2022-09-29 15:31:42 +0200

    Fin Evolution 1

--- routes/Web.php
+++ routes/Web.php
@@ -15,10 +15,13 @@ class Web
     function __construct()
     {
         $main = new SampleWeb();
-        Route::Add('/', [$main, 'home']);
-        Route::Add('/about', [$main, 'about']);
-        Route::Add('/connection', [$main, 'connection']);
-
+        if (SessionHelpers::isLogin()){
+            Route::Add('/', [$main, 'home']);
+            Route::Add('/about', [$main, 'about']);
+        } else {
+            Route::Add('/', [$main, 'connection']);
+            Route::Add('/connection', [$main, 'connection']);
+        }
         $auth = new AuthControler();
         Route::Add('/login', [$auth, 'login']);
 
@@ -28,9 +31,11 @@ class Web
         //        }
 
         $todo = new TodoWeb();
-        Route::Add('/todo/liste', [$todo, 'liste']);
-        Route::Add('/todo/ajouter', [$todo, 'ajouter']);
-        Route::Add('/todo/terminer', [$todo, 'terminer']);
-        Route::Add('/todo/supprimer', [$todo, 'supprimer']);
+        if (SessionHelpers::isLogin()){
+            Route::Add('/todo/liste', [$todo, 'liste']);
+            Route::Add('/todo/ajouter', [$todo, 'ajouter']);
+            Route::Add('/todo/terminer', [$todo, 'terminer']);
+            Route::Add('/todo/supprimer', [$todo, 'supprimer']);
+        }
     }
 }
