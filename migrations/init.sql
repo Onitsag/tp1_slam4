69bfe8bbf3110d2f596041175f39961ee4b59e4b
Timéo Gastineau <tgastineau@dombtsig.local>
2022-09-30 13:10:52 +0200

    Ajout BDD dans migration

--- migrations/init.sql
+++ migrations/init.sql
@@ -1,3 +1,28 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : ven. 30 sep. 2022 à 13:08
+-- Version du serveur : 10.4.24-MariaDB
+-- Version de PHP : 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `tg_slam4tp1`
+--
+
+-- --------------------------------------------------------
+
 --
 -- Structure de la table `todos`
 --
@@ -6,8 +31,37 @@ CREATE TABLE `todos` (
   `id` int(11) NOT NULL,
   `texte` varchar(200) NOT NULL,
   `termine` tinyint(1) NOT NULL DEFAULT 0,
-  `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
+  `login` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `todos`
+--
+
+INSERT INTO `todos` (`id`, `texte`, `termine`, `timestamp`, `login`) VALUES
+(13, 'Note onitsag', 1, '2022-09-29 14:29:35', 'onitsag'),
+(14, 'Note tgastineau', 0, '2022-09-29 14:29:40', 'tgastineau');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `LOGIN` varchar(255) NOT NULL,
+  `PASSWORD` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`LOGIN`, `PASSWORD`) VALUES
+('onitsag', '$2y$10$oZSh.0zEDXQ3lnNs0PxTE.7MHxWAn8/w8CcFx2LWvx/i1nlHvRsLu'),
+('test', '$2y$10$czAqJOC6hdFpexLg35AM6OxOBroTmWubRQ9Nr5821xMA7RSgGWGe.'),
+('tgastineau', '$2y$10$Z0VmXqtzBmASW7nLwSSzZeQgBG1zNE8xLxJiPXzpkucA.PR2hYcSG');
 
 --
 -- Index pour les tables déchargées
@@ -17,7 +71,14 @@ CREATE TABLE `todos` (
 -- Index pour la table `todos`
 --
 ALTER TABLE `todos`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_TODOS_USERS` (`login`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`LOGIN`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -27,5 +88,19 @@ ALTER TABLE `todos`
 -- AUTO_INCREMENT pour la table `todos`
 --
 ALTER TABLE `todos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `todos`
+--
+ALTER TABLE `todos`
+  ADD CONSTRAINT `FK_TODOS_USERS` FOREIGN KEY (`login`) REFERENCES `users` (`LOGIN`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
